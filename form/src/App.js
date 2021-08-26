0831a606de0f5a443bebf3eaf708401df631c968
JuniorMebs <juju.mebs05@gmail.com>
2021-08-26 11:44:10 +0000

    checkpoint jsx

---
+++ form/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './style.css';
+
+function App() {
+  return (
+    <>
+    <div style="border:solid 1px black;max-width:100vw">
+
+<h1 class="title red">JeanBaptiste Junior MEBIAME </h1>
+
+<br/>
+
+<img src="./WhatsApp Image 2021-08-01 at 22.58.54.jpeg" />
+
+<br/>
+
+<img src="./WhatsApp Image 2021-08-06 at 23.52.54.jpeg"/ >
+
+</div>
+
+<video width="320" height="240" contrôles>
+
+<source src="./WhatsApp Video 2021-07-29 at 17.17.49.mp4" type="video/mp4"  />
+
+</video>
+
+</>
+)
+}
+
+export default App;
